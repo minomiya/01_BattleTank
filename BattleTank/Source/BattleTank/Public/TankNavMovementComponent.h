842756cbcf55f03000e9de983c1ff4f79bbce95b
minomiya <minomiyarafael@gmail.com>
2019-03-31 19:02:56 -0300

    add NavMovementComponent and attach the physics material

---
+++ BattleTank/Source/BattleTank/Public/TankNavMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankNavMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankNavMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
