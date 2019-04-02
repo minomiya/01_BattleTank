eab8eeecb9d3bec62afcabc94df8612796912f4a
minomiya <minomiyarafael@gmail.com>
2019-04-02 20:48:37 -0300

    finish the tank AI auto movement

--- BattleTank/Source/BattleTank/Private/TankNavMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankNavMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyrights LateGameStudios Ltd.
 
 #include "TankNavMovementComponent.h"
 #include "TankTrack.h"
@@ -27,11 +27,12 @@ void UTankNavMovementComponent::IntendTurnRight(float Throw)
 
 void UTankNavMovementComponent::RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("RequestDirectMove from %s velocity %s"), *GetOwner()->GetName(), *MoveVelocity.ToString())
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
 
 	float Throw = FVector::DotProduct(TankForward, AIForwardIntention);
+	FVector TurnThrow = FVector::CrossProduct(TankForward, AIForwardIntention);
 
 	IntendMoveForward(Throw);
+	IntendTurnRight(TurnThrow.Z);
 }
--- BattleTank/Source/BattleTank/Public/TankNavMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankNavMovementComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyrights LateGameStudios Ltd.
 
 #pragma once
 
@@ -17,20 +17,21 @@ class BATTLETANK_API UTankNavMovementComponent : public UNavMovementComponent
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = TankInputs)
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 	
-	UFUNCTION(BlueprintCallable, Category = TankInputs)
+	UFUNCTION(BlueprintCallable, Category = "Inputs")
 	void IntendMoveForward(float Throw);
 
-	UFUNCTION(BlueprintCallable, Category = TankInputs)
+	UFUNCTION(BlueprintCallable, Category = "Inputs")
 	void IntendTurnRight(float Throw);
 
-	/** path following: request new velocity */
-	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
-
 private:
+	// Called from the pathfinding logic by the AI controllers
+	void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
+	
 	UTankTrack* LeftTrack = nullptr;
+	
 	UTankTrack* RightTrack = nullptr;
 
 };
